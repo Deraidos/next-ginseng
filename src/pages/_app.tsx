b25e48009fcb368ed68a1a701e744f932bc420f0
Dera <deraidos@zoho.com>
2023-04-26 18:47:33 +0700

    add navbar (temp) & logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <title>GINSENG STRIP 2020</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Ginseng Strip 2020" />
+      <link rel="icon" href="/favicon.png" />
+      <Component {...pageProps} />
+    </>
+  )
 }

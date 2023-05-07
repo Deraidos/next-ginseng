f11f21d0fe6d4697e4d80bf3d26e2224022a43dd
Dera <deraidos@zoho.com>
2023-05-08 00:11:45 +0700

    change black to rgb20

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,15 @@
-import Image from "next/image"
-import { Inconsolata } from "next/font/google"
-import { ShoppingCart } from "@phosphor-icons/react"
-import Link from "next/link"
+import Image from 'next/image'
+import { Inconsolata } from 'next/font/google'
+import { ShoppingCart } from '@phosphor-icons/react'
+import Link from 'next/link'
 
-const inconsolata = Inconsolata({ subsets: ["latin"] })
+const inconsolata = Inconsolata({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main
-      className={`${inconsolata.className} bg-[rgb(20,20,20)] h-[calc(100vh+6rem)]`}
-    >
+    <main className={`${inconsolata.className} bg-[rgb(20,20,20)]`}>
       <section className="flex flex-col" id="megatext__container">
-        <div
-          className="flex p-6 lg:p-12 justify-center items-center"
-          id="megatext__theText"
-        >
+        <div className="megatext__theText flex p-6 lg:p-12 justify-center items-center">
           <Link href="/">
             <Image
               src="/gsText.svg"
@@ -26,13 +21,14 @@ export default function Home() {
         </div>
       </section>
 
-      <header className="nav nav__container bg-[rgb(10,10,10)] h-[80px] w-full text-white flex justify-between items-center text-xl border-y-2">
+      {/* navbar */}
+      <header className="nav nav__container bg-[rgb(20,20,20)] h-[80px] w-full text-white flex justify-between items-center text-xl border-y-2">
         <div className="nav-logo max-w-[80px] m-4 lg:m-32">
           <Image
             src="/white-logo-500px.webp"
             width={500}
             height={500}
-            alt={"ginseng strip logo"}
+            alt={'ginseng strip logo'}
           />
         </div>
         <nav className="nav nav__items h-full flex items-center border-l-2 p-4">
@@ -49,6 +45,18 @@ export default function Home() {
           </ul>
         </nav>
       </header>
+
+      {/* highlight latest products */}
+      <section className="grid__container grid grid-cols-2 ">
+        <div className="bg-[#D97A36]">
+          <h2>2020</h2>
+          <h1>TIKUS GOT</h1>
+        </div>
+
+        <div className="relative">
+          <img src="/catalog/tikusgot-01.jpeg" />
+        </div>
+      </section>
     </main>
   )
 }
